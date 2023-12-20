217e33ca1ddf73d02b55ec8e6c244d82e126706c
Johnwil Delgado <jannwildelgado@gmail.com>
2023-12-21 00:58:53 +0800

    update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,9 +1,11 @@
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+local separator = package.config:sub(1, 1)
+
+if separator == "/" then
+    vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+    vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+    vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+    vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+end
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
