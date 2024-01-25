a5d70059264f3e4c9daf5b3a7b1ec0cf2357dba8
Johnwil Delgado <jannwildelgado@gmail.com>
2024-01-25 13:26:31 +0800

    update keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,14 +1,15 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local separator = package.config:sub(1, 1)
+-- local separator = package.config:sub(1, 1)
 
-if separator == '/' then
-  vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-  vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-  vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-  vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-end
+-- if OS is linux/mac
+-- if separator == '/' then
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+-- end
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
