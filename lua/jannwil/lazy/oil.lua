aff6c5de5a56195cfed3d707e6ffd7c45bfcb431
Johnwil Delgado <jannwildelgado@gmail.com>
2024-08-08 02:49:35 +0800

    switching back to netrw

--- lua/jannwil/lazy/oil.lua
+++ lua/jannwil/lazy/oil.lua
@@ -1,9 +1,9 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function ()
-    require('oil').setup()
-
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', {})
-  end
+  -- 'stevearc/oil.nvim',
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- config = function ()
+  --   require('oil').setup()
+  --
+  --   vim.keymap.set('n', '-', '<CMD>Oil<CR>', {})
+  -- end
 }
