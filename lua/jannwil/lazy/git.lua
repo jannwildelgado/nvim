48b7ddb3d8688dadca4b3d4d5dc1187f9da3b7e3
Johnwil Delgado <jannwildelgado@gmail.com>
2024-09-22 22:40:14 +0800

    git stuff

---
+++ lua/jannwil/lazy/git.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "tpope/vim-fugitive"
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end,
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      }
+    }
+  }
+}
--- lua/jannwil/lazy/init.lua
+++ lua/jannwil/lazy/init.lua
@@ -1,5 +1,4 @@
 return {
-  'tpope/vim-fugitive',
   'christoomey/vim-tmux-navigator',
   -- 'lukas-reineke/indent-blankline.nvim',
 }
