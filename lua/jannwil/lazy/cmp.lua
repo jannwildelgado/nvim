3e8e65ea33b3b2574e77d8a2657989cf3dbbdd80
Johnwil Delgado <jannwildelgado@gmail.com>
2026-01-15 21:22:04 +0800

    blink cmp

--- lua/jannwil/lazy/cmp.lua
+++ lua/jannwil/lazy/cmp.lua
@@ -1,59 +1,65 @@
 return {
-  'hrsh7th/nvim-cmp',
-  dependencies = {
-    'L3MON4D3/LuaSnip',
-    'hrsh7th/cmp-nvim-lsp',
-    'saadparwaiz1/cmp_luasnip',
-    'rafamadriz/friendly-snippets'
-  },
-  config = function()
-    -- [[ Configure nvim-cmp ]]
-    -- See `:help cmp`
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
-    require('luasnip.loaders.from_vscode').lazy_load()
-    luasnip.config.setup {}
-
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' })
-      }),
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }
-      }
-    })
-  end
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+			opts = {},
+		},
+		"folke/lazydev.nvim",
+		{
+			"giuxtaposition/blink-cmp-copilot",
+			dependencies = {
+				{
+					"zbirenbaum/copilot.lua",
+					cmd = "Copilot",
+					event = "InsertEnter",
+					config = function()
+						require("copilot").setup({})
+					end,
+				},
+			},
+		},
+	},
+	opts = {
+		keymap = { preset = "default" },
+		appearance = { nerd_font_variant = "mono" },
+		completion = {
+			documentation = { auto_show = true, auto_show_delay_ms = 200 },
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "lazydev", "copilot" },
+			providers = {
+				lazydev = {
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+					score_offset = 100,
+					async = true,
+				},
+			},
+		},
+		snippets = { preset = "luasnip" },
+		fuzzy = { implementation = "lua" },
+		signature = { enabled = true },
+	},
 }
-
